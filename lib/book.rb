3e75e4f8ca6c630df2aedbad68e7ec5f3f9b4800
jerrybennett <jerrysbusiness@gmail.com>
2017-11-21 23:36:54 -0500

    finish lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  def author
+    @author
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
